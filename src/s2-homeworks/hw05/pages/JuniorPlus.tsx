08f36d5ca7ae93a72626f1c7611cc0b2da3135da
Varya07 <varya_maz@mail.ru>
2023-07-02 20:11:00 +0300

    first commit

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -9,9 +9,9 @@ import HW15 from '../../hw15/HW15';
 function JuniorPlus() {
   return (
     <div id={'hw5-page-junior-plus'}>
-      {/*<HW10/>*/}
-      {/*<HW11/>*/}
-      {/*<HW12/>*/}
+      <HW10/>
+      <HW11/>
+      <HW12/>
       {/*<HW13/>*/}
       {/*<HW14/>*/}
       {/*<HW15 />*/}
